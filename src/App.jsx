901f4a1c4cc63fd0eb6ed94bbea4af4bc80945a7
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-06-20 00:25:44 -0300

    styles done, calculate winner done

--- src/App.jsx
+++ src/App.jsx
@@ -32,30 +32,52 @@ function App() {
     setIsX(!isX)
     console.log(squares[i])
   }
+
+
   return (
 
 
     <div className="App">
-      <div className="row">
+      <h1>tic - tac - toe</h1>
+      <div className="board-wrapper">
+        <svg className='board' width="420" height="420" viewBox="0 0 555 552" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_1_2)">
+            <path d="M166.002 21C166.002 37.0076 165.066 52.9922 165.002 69C164.94 84.3443 166.587 98.9153 168.057 114.111C171.609 150.812 170.505 188.091 168.446 224.889C167.284 245.664 168.002 266.688 168.002 287.5C168.002 313.199 172.495 339.132 175.002 364.667C177.031 385.343 176.845 405.165 170.446 424.944C169.154 428.939 168.002 430.896 168.002 435.056C168.002 440.69 168.895 446.323 169.002 452C169.365 471.254 170.002 490.634 170.002 510" stroke="black" stroke-width="5" stroke-linecap="round" />
+            <path d="M363 25C362.38 31.8248 359.558 38.4844 358.556 45.3333C356.92 56.5126 356.273 67.6004 355.778 78.8889C354.956 97.5968 355.388 116.294 354.778 135C354.225 151.96 352.037 168.69 350.444 185.556C348.559 205.524 350.162 226.062 350 246.111C349.835 266.515 348.51 286.876 349.056 307.333C349.843 336.866 353.619 366.254 355.444 395.722C356.625 414.772 356 433.969 356 453.056C356 472.142 353 490.876 353 510" stroke="black" stroke-width="5" stroke-linecap="round" />
+            <path d="M10 362.324C18.1695 361.916 26.1991 360.829 34.2326 359.902C59.2601 357.017 84.2675 355.032 109.623 353.508C146.372 351.298 183.086 348.944 220.015 348.106C241.046 347.629 261.871 348.913 282.875 348.913C305.564 348.913 328.221 349.435 350.913 349.472C394.038 349.543 437.343 348.843 479.739 353.88C491.614 355.291 502.862 357.542 514.431 359.623C519.343 360.507 527.949 361.256 531 364" stroke="black" stroke-width="5" stroke-linecap="round" />
+            <path d="M0 178.559C36.2331 177.424 72.4751 176.676 108.767 176.128C145.516 175.574 181.742 176.902 218.323 178.449C281.869 181.135 345.682 180.616 409.284 182.647C443.696 183.746 477.736 186.042 511.971 188.006C518.299 188.368 524.641 189 531 189" stroke="black" stroke-width="5" stroke-linecap="round" />
+          </g>
+          <defs>
+            <clipPath id="clip0_1_2">
+              <rect width="555" height="552" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
 
-        <Square value={squares[0]} onClick={() => handleClick(0)} />
-        <Square value={squares[1]} onClick={() => handleClick(1)} />
-        <Square value={squares[2]} onClick={() => handleClick(2)} />
+
+        <div className="back-board">
+          <div className="row">
+
+            <Square value={squares[0]} onClick={() => handleClick(0)} />
+            <Square value={squares[1]} onClick={() => handleClick(1)} />
+            <Square value={squares[2]} onClick={() => handleClick(2)} />
+          </div>
+          <div className="row">
+
+            <Square value={squares[3]} onClick={() => handleClick(3)} />
+            <Square value={squares[4]} onClick={() => handleClick(4)} />
+            <Square value={squares[5]} onClick={() => handleClick(5)} />
+          </div>
+          <div className="row">
+
+
+            <Square value={squares[6]} onClick={() => handleClick(6)} />
+            <Square value={squares[7]} onClick={() => handleClick(7)} />
+            <Square value={squares[8]} onClick={() => handleClick(8)} />
+          </div>
+        </div>
       </div>
-      <div className="row">
-
-        <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
-        <Square value={squares[5]} onClick={() => handleClick(5)} />
-      </div>
-      <div className="row">
-
-
-        <Square value={squares[6]} onClick={() => handleClick(6)} />
-        <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
-      </div>
-
+      <p>Coded by Valentina Peralta</p>
     </div>)
 }
 
--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -6,7 +6,12 @@ function Square({ value, onClick }) {
             className='square'
             onClick={onClick}>
 
-            {value}
+            {value === 'X' ? <svg className='x' width="136" height="136" viewBox="0 0 136 136" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M89 38C88.4279 44.0067 84.3626 49.7552 81.5556 54.8889C73.1335 70.2914 64.5771 85.7182 55.5556 100.778C52.669 105.596 40.8502 132.49 41.2778 111.111C41.6199 94.0066 47.0648 76.5192 53.5556 60.8333C56.1467 54.5714 57.72 55.1013 62.7222 59.3889C73.417 68.5558 83.0486 79.0486 93 89" stroke="#c20404" stroke-width="8" stroke-linecap="round" />
+            </svg> : value === 'O' ? <svg className='o' width="136" height="136" viewBox="0 0 136 136" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M56 38C48.1992 50.699 41.6315 64.3315 37.4444 78.6667C35.9873 83.6556 34.4437 89.1894 35.2222 94.4444C37.1113 107.196 53.767 105.448 62.6111 100.333C74.8241 93.2704 83.185 79.4727 86.1667 66C87.6122 59.4682 89.6944 49.3262 87.8889 42.5556C85.9626 35.332 75.3699 42.6301 73 45" stroke="#0A0262" stroke-width="8" stroke-linecap="round" />
+            </svg> : null
+            }
         </button>
     )
 }
